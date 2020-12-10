ec436e24797e351c69aae75a2907ed4bc7afa56e
Fabiana Zioti <fabiana_zioti@hotmail.com>
2020-12-10 16:14:23 -0300

    updating travis

--- lccs_ws/config.py
+++ lccs_ws/config.py
@@ -25,7 +25,7 @@ class Config:
     CSRF_ENABLED = False
     WTF_CSRF_ENABLED = False
     SECRET_KEY = "APi-Users-123456"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', "postgresql://postgres:mysecretpassword@localhost:5432/lccs")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', "postgresql://user:password@localhost:5432/lccs")
     LCCS_URL = os.getenv('LCCS_URL', 'http://localhost:5000')
     LCCS_UPLOAD_FOLDER = os.getenv('LCCS_UPLOAD_FOLDER', '/path/to/the/uploads')
 
