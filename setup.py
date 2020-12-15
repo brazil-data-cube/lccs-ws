3d4dceab9f66d6224b56df4b1d0a63d112dbd07c
Fabiana Zioti <fabiana_zioti@hotmail.com>
2020-12-15 17:17:50 -0300

    add test dependece

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ tests_require = [
     'pytest-pep8>=1.0',
     'pydocstyle>=4.0',
     'isort>4.3',
-    'check-manifest>=0.40'
+    'check-manifest>=0.40',
+    'requests-mock>=1.7.0',
 ]
 
 docs_require = [
